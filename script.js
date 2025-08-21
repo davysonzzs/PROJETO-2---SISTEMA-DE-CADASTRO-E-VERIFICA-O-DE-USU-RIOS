ef826acd773d04de60f70a38b0958f7d3ff3b7c2
Davyson Levi <davyssonlevi2.0@gmail.com>
2025-08-21 14:23:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+// variaveis do cadrasto
+
+let nomeCadrastro = prompt("Qual o seu nome?").toLowerCase()
+let idadeCadrasto = parseInt(prompt("qual sua idade"))
+let senhaCadrasto = prompt("qual sua senha?").toLowerCase()
+
+// testa se é na idade correta
+if(idadeCadrasto > 0 && idadeCadrasto >= 13){
+    alert(`
+        --- Cadastro de Usuário ---
+        Digite seu nome: ${nomeCadrastro}
+        Digite sua idade: ${idadeCadrasto}
+        Digite uma senha: ${senhaCadrasto}
+
+
+        Cadastro realizado com sucesso!
+        `)
+
+        //se caso for, vai para a parte do login
+    alert("hora do login!")
+    //variaveis do login
+    let nomeLogin = prompt("digite seu nome!").toLowerCase()
+    let senhaLogin = prompt("digite sua senha!").toLowerCase()
+    //teste para ver se os dado foram inseridos corretamente
+    if(nomeLogin === nomeCadrastro && senhaLogin === senhaCadrasto){
+        alert(`
+            --- Login ---
+            Digite seu nome: ${nomeLogin}
+            Digite sua senha: ${senhaLogin}
+
+            Login realizado com sucesso! Bem-vindo, ${nomeCadrastro}.
+            `)
+    } else {
+        //mensegem q mostra se errar o login
+        alert("Nome ou senha incorretos. Tente novamente.")
+    }
+// caso se a idade foi invalida
+} else {
+    alert(`
+        --- Cadastro de Usuário ---
+        Digite seu nome: ${nomeCadrastro}
+        Digite sua idade: ${idadeCadrasto}
+        Digite uma senha: ${senhaCadrasto}
+        
+        Você não pode se cadastrar. Idade mínima: 13 anos.
+        `)
+}
