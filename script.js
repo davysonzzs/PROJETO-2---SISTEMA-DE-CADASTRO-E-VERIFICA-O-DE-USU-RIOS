c8d1d0315a900ae021db3a7523e1243de2f523d1
Davyson Levi <davyssonlevi2.0@gmail.com>
2025-08-22 13:31:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // variaveis do cadrasto
-
+alert("--- Cadastro de Usuário ---")
 let nomeCadrastro = prompt("Qual o seu nome?").toLowerCase()
 let idadeCadrasto = parseInt(prompt("qual sua idade"))
 let senhaCadrasto = prompt("qual sua senha?").toLowerCase()
@@ -7,19 +7,17 @@ let senhaCadrasto = prompt("qual sua senha?").toLowerCase()
 // testa se é na idade correta
 if(idadeCadrasto > 0 && idadeCadrasto >= 13){
     alert(`
-        --- Cadastro de Usuário ---
         Cadastro realizado com sucesso!
         `)
 
         //se caso for, vai para a parte do login
-    alert("hora do login!")
+    alert("--- Login ---")
     //variaveis do login
     let nomeLogin = prompt("digite seu nome!").toLowerCase()
     let senhaLogin = prompt("digite sua senha!").toLowerCase()
     //teste para ver se os dado foram inseridos corretamente
     if(nomeLogin === nomeCadrastro && senhaLogin === senhaCadrasto){
         alert(`
-            --- Login ---
             Login realizado com sucesso! Bem-vindo, ${nomeCadrastro}.
             `)
     } else {
